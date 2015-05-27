f524be3bba1867e4039f00805e84d009bce8f547
aZXe <mikhail.vasenda@gmail.com>
2015-05-27 10:51:42 +0300

    Rough draft.

---
+++ app/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('schedule', [
+  'ngRoute'
+//  'schedule.services'
+//  'myApp.view1',
+//  'myApp.view2',
+//  'myApp.version'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+  	.when('/list', {templateUrl: 'views/list.html', controller: 'ListLessonsCtrl'})
+  	.when('/request', {templateUrl: 'views/request.html'})
+  	.otherwise({redirectTo: '/request'});
+}]);
