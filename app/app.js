391d97ef6813719409731e6ada91a73728a2d5f6
aZXe <mikhail.vasenda@gmail.com>
2015-06-01 09:21:04 +0300

    Refactored controllers.

--- app/app.js
+++ app/app.js
@@ -2,12 +2,13 @@
 
 // Declare app level module which depends on views, and components
 angular.module('schedule', [
-  'ngRoute'
+  'ngRoute',
+  'schedule.controllers'
 //  'schedule.services'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider
-  	.when('/list', {templateUrl: 'views/list.html', controller: ListLessonsCtrl})
+  	.when('/list', {templateUrl: 'views/list.html', controller: 'ListLessonsCtrl'})
   	.when('/request', {templateUrl: 'views/request.html'})
   	.otherwise({redirectTo: '/request'});
 }]);
--- app/controllers.js
+++ app/controllers.js
@@ -1,7 +1,10 @@
 'use strict';
 
-function ListLessonsCtrl($scope, $http) {
-	$http.get('schedule/schedule.json').success(function(data) {
-		$scope.timetables = data;
-	});
-}
+angular.module('schedule.controllers', [])
+    .controller('ListLessonsCtrl', ['$scope', '$http' ,
+        function ($scope, $http) {
+	        $http.get('schedule/schedule.json').success(function(data) {
+		        $scope.timetables = data;
+	        });
+        }
+    ]);
